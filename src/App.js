84b0e9b738f70989fc6bf32bb2f207faf801b1b8
phananhtu1998 <109066304+phananhtu1998@users.noreply.github.com>
2024-02-29 14:11:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          123 456 alo ty dao khung
+          123 456 alo ty dao hat karaoke do
         </p>
         <a
           className="App-link"
